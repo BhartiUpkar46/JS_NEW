e45357358e2bf3853a23f4cca5137108b65709e1
BhartiUpkar46 <bhartiupkar46@gmail.com>
2025-02-24 21:32:57 +0000

    2nd change

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,6 @@
+let score=33
+ let score2="44abe"
+ console.log(typeof score)
+ console.log(typeof(score2))
+ let converted_score=Number(score2)
+ console.table([typeof score,typeof(score2),score,score2,converted_score])
