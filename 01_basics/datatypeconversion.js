a789d42381c3adda4712b8c20ab40f5fb47daa0f
BhartiUpkar46 <bhartiupkar46@gmail.com>
2025-03-07 19:23:35 +0000

    commit4

--- 01_basics/datatypeconversion.js
+++ 01_basics/datatypeconversion.js
@@ -1,6 +1,32 @@
-let score=33
- let score2="44abe"
- console.log(typeof score)
- console.log(typeof(score2))
- let converted_score=Number(score2)
- console.table([typeof score,typeof(score2),score,score2,converted_score])
+// // let score=33
+// //  let score2="44abe"
+// //  console.log(typeof score)
+// //  console.log(typeof(score2))
+// //  let converted_score=Number(score2)
+// //  console.table([typeof score,typeof(score2),score,score2,converted_score])
+
+
+
+
+
+//  /* Operations */
+// let str1="jit ";
+// let str2="mit"
+// let str3=str1+str2
+// console.log(1+2+4);
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+// console.log(true)
+console.log(+true)
+
+console.log(2>1)
+console.log(typeof(null))
+console.log(typeof(undefined))
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined>=0)
+console.log(undefined<=0)
+console.log(null<0)
+console.log(null>0)
+console.log(null>=0)
+console.log(null==0)
